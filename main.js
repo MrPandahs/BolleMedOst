575ea548d30646e8eee91ade9085125ae417a308
chris <christofferbzh@gmail.com>
2025-11-12 11:41:47 +0100

    added chart

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
-const verticalPie = document.querySelector('#vertical-bar').getContext('2d');
-const chart = new Chart(verticalPie, {
-    type: "line",
+const horiztontalBar = document.querySelector('#horizontal-bar').getContext('2d');
+const chart = new Chart(horiztontalBar, {
+    type: "bar",
     data: {
-    datasets: [{
-    label: "Alice\n",
-        data: []
+        datasets: [
+            {
+                label: "Alice",
+                data: [4.00, 6.00, 6.00]
+            },
+            {
+                label: "Amar",
+                data: [4.00, 5.00, 3.00]
+            },
+            {
+                label: "Andersen Bakery",
+                data: [6.00, 4.00, 3.00]
+            },
+            {
+                label: "Il Buco",
+                data: [6.00, 5.00, 5.00]
+            },
+            {
+                label: "Kasada",
+                data: [4.50, 5.50, 5.00]
+            },
+            {
+                label: "Sneezing Fruits",
+                data: [6.00, 6.00, 5.00]
+            },
+            {
+                label: "Yellow Bird Coffee",
+                data: [3.50, 4.50, 6.00]
+            }],
+        labels: ["Bolle", "Smør", "Ost"]
     },
-        {
-            label: "Amar",
-            data: []
-        },
-        {
-            label: "Andersen Bakery",
-            data: []
-        },
-        {
-            label: "Il Buco",
-            data: []
-        },
-        {
-            label: "Kasada",
-            data: []
-        },
-        {
-            label: "Sneezing Fruits",
-            data: []
-        },
-        {
-            label: "Yellow Bird Coffee",
-            data: []
-    }]
+    options: {
+        indexAxis: 'y'
     }
 })
+
+const bolle = document.querySelector('#bolle')
+const smør = document.querySelector('#smør')
+const ost = document.querySelector('#ost')
+
+const ingredientChecked = async function()  {
+
+}
+ingredientChecked()
